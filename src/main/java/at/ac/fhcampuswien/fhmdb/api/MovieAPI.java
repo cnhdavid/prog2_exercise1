5b6e9186b7c71d19f0c0dbf245e971542a59abb4
Jass2909 <jasmeet@gmx.at>
2024-04-07 18:56:38 +0200

    Added okhttp

--- src/main/java/at/ac/fhcampuswien/fhmdb/api/MovieAPI.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/api/MovieAPI.java
@@ -1,6 +1,8 @@
 package at.ac.fhcampuswien.fhmdb.api;
-
+import okhttp3.OkHttpClient;
 public class MovieAPI {
+    private final OkHttpClient client = new OkHttpClient();
+
 
 }
 
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module at.ac.fhcampuswien.fhmdb {
     requires javafx.fxml;
 
     requires com.jfoenix;
+    requires okhttp3;
 
     opens at.ac.fhcampuswien.fhmdb;
     exports at.ac.fhcampuswien.fhmdb;
